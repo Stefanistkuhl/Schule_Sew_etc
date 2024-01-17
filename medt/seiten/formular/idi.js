cd18ac169409f6bc4c7efaa7f10a0cf76d2ef66d
Stefanistkuhl <ghgstefan@gmail.com>
2024-01-17 17:20:14 +0100

    asfasfsdgsdgsdg

--- medt/seiten/formular/idi.js
+++ medt/seiten/formular/idi.js
@@ -1,10 +1,23 @@
-function abschicken(){
+//der fix war es die funktion in eine variable zu geben quelle:https://www.youtube.com/watch?v=WOAggxx8Sjs
+var ichhassejs = function (){
+    var geschlecht = "";
+    if(document.getElementById("m").checked){
+            geschlecht=document.getElementById("m").value
+    }
+    if(document.getElementById("f").checked){
+        geschlecht=document.getElementById("f").value
+    }
+    if(document.getElementById("n").checked){
+        geschlecht=document.getElementById("n").value
+    }
     alert(
-        document.form.firstname.value + " " +    
-        document.form.firstname.value
+        document.getElementById("fn").value + " \n" +    
+        document.getElementById("ln").value + " \n" +   
+        document.getElementById("em").value + " \n" +
+        document.getElementById("bd").value + " \n" +
+        geschlecht + " \n" +
+        document.getElementById("li").value
+        
     );
-    alert(
-        document.querySelector("fn").value + " " +    
-        document.querySelector("ln").value
-    );
+    
 }
