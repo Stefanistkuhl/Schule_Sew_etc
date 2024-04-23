4542a8a2933bc03cd4c246647a96537cdb5189d1
stefiii <stefanundervover20@gmail.com>
2024-04-23 13:27:53 +0200

    oida

--- sew/2kl/guitaktoe/src/jsdhofsdijf/gfbdk.java
+++ sew/2kl/guitaktoe/src/jsdhofsdijf/gfbdk.java
@@ -22,15 +22,25 @@ public class gfbdk {
 
     boolean dransein = false;
 
-    public gfbdk() {
-        ArrayList<Boolean>top=new ArrayList<>();
-        ArrayList<Boolean>mid=new ArrayList<>();
-        ArrayList<Boolean>bottom=new ArrayList<>();
+    ArrayList<Integer> top = new ArrayList<>();
+    ArrayList<Integer> mid = new ArrayList<>();
+    ArrayList<Integer> bottom = new ArrayList<>();
 
+    public gfbdk() {
+
+        top.add(0);
+        top.add(0);
+        top.add(0);
+        mid.add(0);
+        mid.add(0);
+        mid.add(0);
+        bottom.add(0);
+        bottom.add(0);
+        bottom.add(0);
         resetButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                zrk();
             }
         });
         exitButton.addActionListener(new ActionListener() {
@@ -42,14 +52,18 @@ public class gfbdk {
         inksoben.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!dransein){
-                   inksoben.setText("X");
-                   spieler.setText("Spieler 2 ist dran");
-                   dransein = true;
-                }else {
+                if (!dransein) {
+                    inksoben.setText("X");
+                    spieler.setText("Spieler 2 ist dran");
+                    dransein = true;
+                    top.set(0, 1);
+                    checkwin();
+                } else {
                     inksoben.setText("0");
                     dransein = false;
                     spieler.setText("Spieler 1 ist dran");
+                    top.set(1, 2);
+                    checkwin();
                 }
             }
         });
@@ -57,14 +71,18 @@ public class gfbdk {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                if (!dransein){
+                if (!dransein) {
                     oben.setText("X");
                     spieler.setText("Spieler 2 ist dran");
                     dransein = true;
-                }else {
+                    top.set(2, 1);
+                    checkwin();
+                } else {
                     oben.setText("0");
                     dransein = false;
                     spieler.setText("Spieler 1 ist dran");
+                    top.add(2);
+                    checkwin();
                 }
             }
         });
@@ -72,28 +90,36 @@ public class gfbdk {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                if (!dransein){
+                if (!dransein) {
                     obenrechts.setText("X");
                     spieler.setText("Spieler 2 ist dran");
                     dransein = true;
-                }else {
+                    top.set(0, 1);
+                    checkwin();
+                } else {
                     obenrechts.setText("0");
                     dransein = false;
                     spieler.setText("Spieler 1 ist dran");
+                    top.set(1, 2);
+                    checkwin();
                 }
             }
         });
         mittelinks.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!dransein){
+                if (!dransein) {
                     mittelinks.setText("X");
                     spieler.setText("Spieler 2 ist dran");
                     dransein = true;
-                }else {
+                    mid.set(1, 1);
+                    checkwin();
+                } else {
                     mittelinks.setText("0");
                     dransein = false;
                     spieler.setText("Spieler 1 ist dran");
+                    mid.set(2, 2);
+                    checkwin();
                 }
 
             }
@@ -101,14 +127,18 @@ public class gfbdk {
         mitte.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!dransein){
-                    obenrechts.setText("X");
+                if (!dransein) {
+                    mitte.setText("X");
                     spieler.setText("Spieler 2 ist dran");
                     dransein = true;
-                }else {
-                    obenrechts.setText("0");
+                    mid.set(0, 1);
+                    checkwin();
+                } else {
+                    mitte.setText("0");
                     dransein = false;
                     spieler.setText("Spieler 1 ist dran");
+                    mid.set(1, 2);
+                    checkwin();
                 }
 
             }
@@ -116,14 +146,18 @@ public class gfbdk {
         mitterechts.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!dransein){
+                if (!dransein) {
                     obenrechts.setText("X");
                     spieler.setText("Spieler 2 ist dran");
                     dransein = true;
-                }else {
-                    obenrechts.setText("0");
+                    mid.set(2, 1);
+                    checkwin();
+                } else {
+                    mitterechts.setText("0");
                     dransein = false;
                     spieler.setText("Spieler 1 ist dran");
+                    mid.set(2, 2);
+                    checkwin();
                 }
 
             }
@@ -131,14 +165,18 @@ public class gfbdk {
         linksunten.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!dransein){
-                    obenrechts.setText("X");
+                if (!dransein) {
+                    linksunten.setText("X");
                     spieler.setText("Spieler 2 ist dran");
                     dransein = true;
-                }else {
-                    obenrechts.setText("0");
+                    bottom.set(0, 1);
+                    checkwin();
+                } else {
+                    linksunten.setText("0");
                     dransein = false;
                     spieler.setText("Spieler 1 ist dran");
+                    bottom.set(0, 2);
+                    checkwin();
                 }
 
             }
@@ -146,14 +184,18 @@ public class gfbdk {
         mitteunten.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!dransein){
-                    obenrechts.setText("X");
+                if (!dransein) {
+                    mitteunten.setText("X");
                     spieler.setText("Spieler 2 ist dran");
                     dransein = true;
-                }else {
-                    obenrechts.setText("0");
+                    bottom.set(0, 1);
+                    checkwin();
+                } else {
+                    mitteunten.setText("0");
                     dransein = false;
                     spieler.setText("Spieler 1 ist dran");
+                    bottom.set(0, 2);
+                    checkwin();
                 }
 
             }
@@ -161,14 +203,18 @@ public class gfbdk {
         rechtsunten.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!dransein){
-                    obenrechts.setText("X");
+                if (!dransein) {
+                    rechtsunten.setText("X");
                     spieler.setText("Spieler 2 ist dran");
                     dransein = true;
-                }else {
-                    obenrechts.setText("0");
+                    bottom.set(1, 1);
+                    checkwin();
+                } else {
+                    rechtsunten.setText("0");
                     dransein = false;
                     spieler.setText("Spieler 1 ist dran");
+                    bottom.set(1, 2);
+                    checkwin();
                 }
 
             }
@@ -182,5 +228,59 @@ public class gfbdk {
         frame.setVisible(true);
     }
 
+    public void zrk() {
+        spieler.setText("Spieler 1 ist dran");
+        dransein = false;
+        inksoben.setText("");
+        oben.setText("");
+        obenrechts.setText("");
+        mittelinks.setText("");
+        mitte.setText("");
+        mitterechts.setText("");
+        rechtsunten.setText("");
+        mitteunten.setText("");
+        linksunten.setText("");
+        top.clear();
+        mid.clear();
+        bottom.clear();
+        bottom.add(0);
+        bottom.add(0);
+        mid.add(0);
+        mid.add(0);
+        mid.add(0);
+        top.add(0);
+        top.add(0);
+        top.add(0);
+    }
 
+    public int checkwin() {
+        int ergebnis = 0;
+        if ((top.get(0) == 1 && top.get(1) == 1 && top.get(2) == 1)
+                || (mid.get(0) == 1 && mid.get(1) == 1 && mid.get(2) == 1)
+                || (bottom.get(0) == 1 && bottom.get(1) == 1 && bottom.get(2) == 1)
+                || ((top.get(0) == 1 && mid.get(0) == 1 && bottom.get(0) == 1)
+                        || ((top.get(1) == 1 && mid.get(1) == 1 && bottom.get(1) == 1)
+                                || (top.get(2) == 1 && mid.get(2) == 1 && bottom.get(2) == 1))
+                        || (top.get(0) == 1 && mid.get(1) == 1 && bottom.get(2) == 1)
+                        || (top.get(2) == 1 && mid.get(1) == 1 && bottom.get(0) == 1))) {
+            ergebnis = 1;
+            System.out.println(ergebnis);
+            spieler.setText("Spieler 1 hat gewonnen");
+        } else if ((top.get(0) == 2 && top.get(1) == 2 && top.get(2) == 2)
+                || (mid.get(0) == 2 && mid.get(1) == 2 && mid.get(2) == 2)
+                || (bottom.get(0) == 2 && bottom.get(1) == 2 && bottom.get(2) == 2)
+                || ((top.get(0) == 2 && mid.get(0) == 2 && bottom.get(0) == 2)
+                        || ((top.get(1) == 2 && mid.get(1) == 2 && bottom.get(1) == 2)
+                                || (top.get(2) == 2 && mid.get(2) == 2 && bottom.get(2) == 2))
+                        || (top.get(0) == 2 && mid.get(1) == 2 && bottom.get(2) == 2)
+                        || (top.get(2) == 2 && mid.get(1) == 2 && bottom.get(0) == 2))) {
+            ergebnis = 2;
+            System.out.println(ergebnis);
+            spieler.setText("Spieler 2 hat gewonnen");
+        } else {
+            ergebnis = 0;
+            System.out.println(ergebnis);
+        }
+        return ergebnis;
+    }
 }
