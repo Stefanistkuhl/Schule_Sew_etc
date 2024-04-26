3834ffc32c53cf2daec1a59c5d28b63a689f46d4
Stefanistkuhl <stefiii@DESKTOP-DSF5MB7>
2024-04-26 20:42:23 +0200

    ass dings historgraam

--- medt/histrogramm/main.py
+++ medt/histrogramm/main.py
@@ -1,17 +1,25 @@
 from PIL import Image
-import matplotlib as plt
-import os
-from math import sqrt
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
-brightness = []
+anz = np.zeros(256)
+bright = np.arange(0,256, dtype=int)
+
 img = Image.open("test.jpg")
 width, height = img.size
 pixels = width * height
-print(width, height)
 for x in range(width):
     for y in range(height):
         r, g, b = img.getpixel((x, y))
-        brightness.append(sum([r, g, b])/3)
+        brightnes = sum([r,g,b])//3
+        anz[brightnes] += 1
 
-plt.bar(brightness,)
-plt.show
+df = pd.DataFrame({'bright':bright, 'anz':anz}, columns=['bright','anz'])
+print(df)
+sns.set_style("darkgrid")
+dig = sns.lineplot(x='bright', y='anz', data=df)
+dig_fig =dig.get_figure()
+dig_fig.savefig('plot.png')
+
