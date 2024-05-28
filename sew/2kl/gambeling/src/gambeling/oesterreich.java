ef4096791587fa5b32e1fe8e52fae1fe26fb02bc
Stefanistkuhl <ghgstefan@gmail.com>
2024-05-28 11:06:43 +0200

    tigerianische tiger trinken bananensaft. Barbara geht in einer bar mit Barbaren und bestellt Rabarber

--- sew/2kl/gambeling/src/gambeling/oesterreich.java
+++ sew/2kl/gambeling/src/gambeling/oesterreich.java
@@ -6,18 +6,41 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class oesterreich {
-    private JButton anzeigen;
+    private JButton skrupellos;
     private JLabel bild1;
     private JLabel bild2;
     private JLabel bild3;
     private JPanel panel1;
+    private JLabel baum;
+    // randoom dings mache
+    // gewonne oder verloren
+    // statistik
+    // bilder austaucen (fake animaion)
 
     public oesterreich() {
-        anzeigen.addActionListener(new ActionListener() {
-            @Override
+        skrupellos.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                Image image = new ImageIcon("car1.png").getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT);
-                bild1.setIcon(new ImageIcon(image));
+                int auto = (int) (Math.random() * 3 + 1);
+                System.out.println(auto);
+                Image[] cars = new Image[3];
+                cars[0] = new ImageIcon("car1.png").getImage()
+                        .getScaledInstance(300, 300,
+                                Image.SCALE_DEFAULT);
+                cars[1] = new ImageIcon("car2.png").getImage()
+                        .getScaledInstance(300, 300,
+                                Image.SCALE_DEFAULT);
+                cars[2] = new ImageIcon("car3.png").getImage()
+                        .getScaledInstance(300, 300,
+                                Image.SCALE_DEFAULT);
+                JLabel[] labels = new JLabel[3];
+                labels[0] = bild1;
+                labels[1] = bild2;
+                labels[2] = bild3;
+
+                for (int feuerwehr = 0; feuerwehr < 3; feuerwehr += 1) {
+                    int rand = (int) (Math.random() * 3) + 1;
+                    labels[rand].setIcon(new ImageIcon(cars[feuerwehr]));
+                }
             }
         });
     }
@@ -26,7 +49,7 @@ public class oesterreich {
         JFrame frame = new JFrame("ka oida");
         frame.setContentPane((new oesterreich().panel1));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(690, 690);
+        frame.setSize(960, 690);
         frame.setVisible(true);
     }
 }
